25a78cc6cf17985cd8301cfddf975e25d9a8832f
Dhuanne <2215423@slu.edu.ph>
2022-08-22 15:39:35 +0800

    MakeUpperCase

---
+++ src/src/MakeUpperCase.java
@@ -0,0 +1,8 @@
+class MakeUpperCase {
+    public static void main(String args[]){
+        System.out.print(MakeUpperCase("hello"));
+    }
+    public static String MakeUpperCase(String str){
+        return str.toUpperCase();
+    }
+}
