b73805c9cccb29c11daa903ec9bd95283f2a0cc3
Dhuanne <2215423@slu.edu.ph>
2022-08-24 15:35:51 +0800

    1

---
+++ src/src/Multiply.java
@@ -0,0 +1,8 @@
+public class Multiply {
+    public static Double multiply(Double a, Double b) {
+        return a * b;
+    }
+    public static void main(String args[]){
+        System.out.println(multiply(6.5,2.3));
+    }
+}
