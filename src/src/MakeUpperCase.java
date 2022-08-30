79346b12893ced153d650c503941fa4026eef743
Kent <2215422@slu.edu.ph>
2022-08-31 01:49:19 +0800

    4 Codes added - Kent

--- src/src/MakeUpperCase.java
+++ src/src/MakeUpperCase.java
@@ -1,11 +1,12 @@
 /**
  * Write a function which converts the input string to uppercase.
- * */
+ */
 class MakeUpperCase {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         System.out.print(MakeUpperCase("hello"));
     }
-    public static String MakeUpperCase(String str){
+
+    public static String MakeUpperCase(String str) {
         return str.toUpperCase();
     }
 }
---
+++ src/src/RemoveExclamationMarks.java
@@ -0,0 +1,11 @@
+/**
+ * DESCRIPTION:
+ * Write function RemoveExclamationMarks which removes all exclamation marks
+ * from a given string.
+ */
+
+class RemoveExclamationMarks {
+    static String removeExclamationMarks(String s) {
+        return s.replaceAll("!", "");
+    }
+}
